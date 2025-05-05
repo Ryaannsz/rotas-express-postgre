0151e4acf0e29fec6db4c5945de81a38d1bb5d4c
Ryan Carvalho Bernardo <106412618+Ryaannsz@users.noreply.github.com>
2025-05-05 20:33:28 +0000

    feat: delete entity

--- src/controllers/contato.controller.js
+++ src/controllers/contato.controller.js
@@ -11,8 +11,15 @@ const getEntitiesById = async(id, res) =>{
 }
 
 const putEntity = async (updatedData, id, res) => {
-    
+    if(!id) return res.status(500).json({message: "Id não informado!"})
+    if(!updatedData) return res.status(500).json({message: "Valores não passados!"})
+    contatoService.putContact(updatedData, id, res)
+}
+
+const deleteEntity = async(email, res) => {
+    if(!email) return res.status(500).json({message: "E-mail não informado!"})
+        contatoService.deleteContact(email, res)
 }
 
 
-export default {getEntities, getEntitiesById}
+export default {getEntities, getEntitiesById, putEntity, deleteEntity}
